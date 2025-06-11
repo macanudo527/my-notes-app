00b7df38f326f1c9f61999bb2fa8e69c8489653d
Neal Chambers <macanudo@yahoo.com>
2025-06-11 16:15:19 +0900

    Finish Step 5

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
@@ -20,9 +21,9 @@ void main() {
     ),
     home: const HomePage(),
     routes: {
-      '/login/': (context) => const LoginView(),
-      '/notes/': (context) => const NotesView(),
-      '/register/': (context) => const RegisterView()
+      loginRoute: (context) => const LoginView(),
+      notesRoute: (context) => const NotesView(),
+      registerRoute: (context) => const RegisterView()
     }
   ));
 }
@@ -82,7 +83,7 @@ class _NotesViewState extends State<NotesView> {
                     if (value) {
                       FirebaseAuth.instance.signOut();
                       Navigator.of(context).pushNamedAndRemoveUntil(
-                        '/login', 
+                        loginRoute, 
                         (_) => false
                       );
                     }
